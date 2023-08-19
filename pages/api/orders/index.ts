921e4f20235a4956e35ff99a0247ccec0658a191
nachopedraza1 <nachi_choco@live.com.ar>
2023-08-19 16:09:52 -0300

    remove api order

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { db, dbProduct } from '@/database';
+import { db } from '@/database';
 import { Order, Product } from '@/models';
 import { IOrder, IOrderItem, IUser } from '@/interfaces';
 import { getServerSession } from 'next-auth';
@@ -25,11 +25,6 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     const { orderItems, total } = req.body as IOrder;
 
-    const session: any = await getServerSession(req, res, authOptions);
-    if (!session) {
-        return res.status(401).json({ message: 'Debe estar autenticado para enviar una orden' });
-    }
-
     const productsIds = orderItems.map(product => product._id);
 
     await db.connect();
@@ -38,31 +33,15 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     try {
         const subTotal = orderItems.reduce((prev, current) => {
-            const currentPrice = dbProducts.find(product => product.id === current._id)!.price;
-            if (!currentPrice) {
-                throw new Error('Verifique el carrito de nuevo, producto no existe.')
-            }
-            return (currentPrice * current.quantity) + prev;
+            
+
+            return (current.price * current.quantity) + prev;
         }, 0)
+    } catch (error) {
 
-        const taxRate = Number(process.env.NEXT_PUBLIC_IVA_RATE || 0);
-        const totalBackend = subTotal * (taxRate + 1);
-
-        if (total !== totalBackend) {
-            throw new Error('El total no cuadra con el monto.')
-        }
-
-        const userId = session.user._id;
-        const newOrder = new Order({ ...req.body, isPaid: false, user: userId })
-        await newOrder.save();
-        return res.status(201).json(newOrder)
-
-    } catch (error: any) {
-        await db.disconnect();
-        console.log(error);
-        res.status(400).json(error.message || 'Revise logs del servidor')
     }
 
 
-    res.status(200).json(req.body)
+    return res.status(201).json({ message: 'asd' })
+
 }
